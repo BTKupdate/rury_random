5f7fa3b4296b1d3abbb350b67739858539c1f47a
BTKupdate <btkupdate@gmail.com>
2023-07-03 08:27:55 +1000

    First upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+author 'BTKupdate (Rury) | Discord: rury'
+description 'Random reward generator'
+version '1.0.0'
+server_only "yes"
+server_scripts {
+	'server/main.lua'
+}
---
+++ server/main.lua
@@ -0,0 +1,49 @@
+local rnd = {}
+local bonusNames = {
+    ['prem'] = true,
+    ['high'] = true,
+    ['med'] = true,
+    ['rare'] = true,
+    ['rarer'] = true,
+    ['risky'] = true,
+    ['unbelieveable'] = true,
+    ['imposible'] = true
+}
+
+local getRandomRewardWithCounts = function(rndTab, rndBonus)
+    local randomTable = rnd.create(rndTab.chance, rndBonus)
+    local rewardType = rnd.tab(randomTable)
+    local rewardIndex = math.random(1, #rndTab.rewards[rewardType])
+    local rewardName = rndTab.rewards[rewardType][rewardIndex][1]
+    local randomCount = rnd.tab(rnd.create(rndTab.rewards[rewardType][rewardIndex][2]))
+    return rewardName, randomCount
+end
+
+local getRandomReward = function(rndTab, rndBonus)
+    local randomTable = rnd.create(rndTab.chance, rndBonus)
+    local rewardType = rnd.tab(randomTable)
+    local rewardIndex = math.random(1, #rndTab.rewards[rewardType])
+    local rewardName = rndTab.rewards[rewardType][rewardIndex]
+    return rewardName
+end
+
+rnd.tab = function( t )
+    return t[ math.random( 1, #t ) ]
+end
+
+rnd.create = function(t, rndBonus)
+    local res = {}
+    local count = 0
+    for p1, p2 in pairs(t) do
+        if rndBonus and bonusNames[p1] then
+            p2 = math.ceil(p2+(p2*rndBonus))
+        end
+        for k = 1, p2 do
+            res[#res+1] = p1
+        end
+    end
+    return res
+end
+
+exports("getRandomRewardWithCounts", getRandomRewardWithCounts)
+exports("getRandomReward", getRandomReward)
